bd2d99ceba7f5e8e6dcebafdefc35cdb1af3ffef
KhoaNguyen <anhkhoa515.dev@gmail.com>
2025-04-28 19:28:31 +0700

    feat ✨  add format function

---
+++ src/utils/formatter.js
@@ -0,0 +1,19 @@
+export const formatNumber = (num) => {
+  return Intl.NumberFormat('en-US', {
+    notation: 'compact',
+    maximumFractionDigits: 1,
+  }).format(num);
+};
+
+export const formatFileSize = (size) => {
+  let i = Math.floor(Math.log(size) / Math.log(1024));
+  return `${(size / Math.pow(1024, i)).toFixed(1)} ${['B', 'KB', 'MB', 'GB', 'TB'][i]}`;
+};
+
+// You can add more formatting functions here
+export const formatPrice = (price) => {
+  return new Intl.NumberFormat('vi-VN', {
+    style: 'currency',
+    currency: 'VND',
+  }).format(price);
+};
