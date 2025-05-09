355436b652b4e86af8e97c4deb7871bdb9043f41
KhoaNguyen <anhkhoa515.dev@gmail.com>
2025-05-10 00:28:16 +0700

    chore: add projects content

--- src/data/data.js
+++ src/data/data.js
@@ -1,9 +1,12 @@
 import servicesData from './services.json';
 import pricingData from './pricing.json';
 import devicesData from './rental.json';
+import projectsData from './projects.json';
 
 export const services = servicesData;
 
 export const pricing = pricingData;
 
 export const devices = devicesData;
+
+export const projects = projectsData;
